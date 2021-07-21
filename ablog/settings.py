cd6e3e7b533f406e83b905b21ae8cc42840eda6b
Abhijeet Veer <veerabhijeet10@gmail.com>
2021-07-21 13:15:01 +0530

    user added

--- ablog/settings.py
+++ ablog/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'theblog',
     'members',
-    
 ]
 
 MIDDLEWARE = [
--- members/views.py
+++ members/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
 from django.urls import reverse_lazy
 
 class UserRegisterView(generic.CreateView):
-    form_class=UserCreationForm
+    form_class=SignUpForm
     template_name='registration/register.html'
     success_url=reverse_lazy('login')
 
